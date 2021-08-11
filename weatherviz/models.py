be6af4707b13333ccbb9e059711120a43c76e426
Zainab Lawal <zainablaw1012@gmail.com>
2021-08-11 21:16:17 +0300

    Created a model for the city data

--- weatherviz/models.py
+++ weatherviz/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.translations import gettext_lazy as _
 
-# Create your models here.
+
+class OpenWeatherCity(models.Model):
+    name = models.CharField(
+        verbose_name=_('City Name'),
+        max_length=150, null=False, blank=False
+        )
+    country = models.CharField(
+        verbose_name=_('Country'),
+        max_length=10, null=False, blank=False
+        )
+    open_weather_id = models.IntegerField(
+        verbose_name=_('Open Weather ID'),
+        null=False, blank= False
+        )
+
+    class Meta:
+        verbose_name=_('Open Weather City')
+        verbose_name_plural=_('Open Weather Cities')
